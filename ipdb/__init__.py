a8f2a3904297f38ebe962fc87582f63422547da0
Godefroid Chapelle <gotcha@bubblenet.be>
2011-06-10 16:38:59 +0200

    allow python -m ipdb module

--- ipdb/__init__.py
+++ ipdb/__init__.py
@@ -1,26 +1,5 @@
-import sys
-from IPython.Debugger import Pdb
-from IPython.Shell import IPShell
-from IPython import ipapi
+from __main__ import set_trace, post_mortem, pm
 
-shell = IPShell(argv=[''])
-
-
-def set_trace():
-    ip = ipapi.get()
-    def_colors = ip.options.colors
-    Pdb(def_colors).set_trace(sys._getframe().f_back)
-
-
-def post_mortem(tb):
-    ip = ipapi.get()
-    def_colors = ip.options.colors
-    p = Pdb(def_colors)
-    p.reset()
-    while tb.tb_next is not None:
-        tb = tb.tb_next
-    p.interaction(tb.tb_frame, tb)
-
-
-def pm():
-    post_mortem(sys.last_traceback)
+pm           # please pyflakes
+post_mortem  # please pyflakes
+set_trace    # please pyflakes
---
+++ manual_test.py
@@ -0,0 +1,10 @@
+def output(arg):
+    print "MANUAL: arg=", arg
+
+
+def main():
+    for abc in range(10):
+        output(abc)
+
+if __name__ == "__main__":
+    main()
