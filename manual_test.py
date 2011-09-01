d0c282f52582cd613ad3beba2abd3f432747be21
Godefroid Chapelle <gotcha@bubblenet.be>
2011-09-01 09:10:23 +0200

    code to test nose

--- manual_test.py
+++ manual_test.py
@@ -3,9 +3,19 @@ def output(arg):
 
 
 def main():
-    #import ipdb; ipdb.set_trace()
     for abc in range(10):
+        import ipdb; ipdb.set_trace()
         output(abc)
+
+
+# code to test with nose
+import unittest
+
+
+class IpdbUsageTests(unittest.TestCase):
+
+    def testMain(self):
+        main()
 
 if __name__ == "__main__":
     main()
