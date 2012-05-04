edea61ce5fae87c9b7940e29ff69514ef1f55eff
Laurent Peuch <cortex@worlddomination.be>
2012-05-04 02:20:52 +0200

    fancy, align comments in __init__.py

--- ipdb/__init__.py
+++ ipdb/__init__.py
@@ -1,9 +1,9 @@
 from ipdb.__main__ import set_trace, post_mortem, pm, run, runcall, runeval, launch_ipdb_on_exception
 
-pm           # please pyflakes
-post_mortem  # please pyflakes
-run          # please pyflakes
-runcall      # please pyflakes
-runeval      # please pyflakes
-set_trace    # please pyflakes
+pm                       # please pyflakes
+post_mortem              # please pyflakes
+run                      # please pyflakes
+runcall                  # please pyflakes
+runeval                  # please pyflakes
+set_trace                # please pyflakes
 launch_ipdb_on_exception # please pyflakes
