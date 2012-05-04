491b46df047903b887f5840aa4f399882563b451
Laurent Peuch <cortex@worlddomination.be>
2012-05-04 02:19:50 +0200

    make launch ipdb_on_exception importable

--- ipdb/__init__.py
+++ ipdb/__init__.py
@@ -1,4 +1,4 @@
-from ipdb.__main__ import set_trace, post_mortem, pm, run, runcall, runeval
+from ipdb.__main__ import set_trace, post_mortem, pm, run, runcall, runeval, launch_ipdb_on_exception
 
 pm           # please pyflakes
 post_mortem  # please pyflakes
@@ -6,3 +6,4 @@ run          # please pyflakes
 runcall      # please pyflakes
 runeval      # please pyflakes
 set_trace    # please pyflakes
+launch_ipdb_on_exception # please pyflakes
