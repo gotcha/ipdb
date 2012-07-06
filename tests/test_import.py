47a817733ddd7e77aee1203d1165bcaaf8dca7eb
Godefroid Chapelle <gotcha@bubblenet.be>
2012-07-06 12:02:35 +0200

    pyflakes and pep8

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,19 +1,26 @@
 # Copyright (c) 2012 Marc Abramowitz
-# 
+#
 # This file is part of ipdb.
 # GNU package is free software: you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free 
-# Software Foundation, either version 2 of the License, or (at your option) 
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 2 of the License, or (at your option)
 # any later version.
 #
-# GNU package is distributed in the hope that it will be useful, but 
+# GNU package is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
 # or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
 # for more details.
 
 import unittest
 
+
 class ImportTest(unittest.TestCase):
 
     def test_import(self):
         from ipdb import set_trace, post_mortem, pm, run, runcall, runeval
+        set_trace  # please pyflakes
+        post_mortem  # please pyflakes
+        pm  # please pyflakes
+        run  # please pyflakes
+        runcall  # please pyflakes
+        runeval  # please pyflakes
