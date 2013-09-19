52daada199abe589cc51892cc32d1df4f1bbdccf
Godefroid Chapelle <gotcha@bubblenet.be>
2013-09-19 15:33:02 +0200

    Preparing release 0.7.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 from setuptools import setup, find_packages
 from sys import version_info
 
-version = '0.7.1dev'
+version = '0.7.1'
 
 long_description = (open('README.rst').read() +
     '\n\n' + open('HISTORY.txt').read())
