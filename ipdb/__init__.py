ca0f08b9fe81a914525951e2d189adc0eb1b2861
Godefroid Chapelle <gotcha@bubblenet.be>
2016-04-29 18:29:57 +0200

    please pyflakes
    simpler

--- ipdb/__init__.py
+++ ipdb/__init__.py
@@ -4,19 +4,8 @@
 # Redistributable under the revised BSD license
 # https://opensource.org/licenses/BSD-3-Clause
 
-from ipdb.__main__ import set_trace, post_mortem, pm, run, runcall, runeval, launch_ipdb_on_exception
+from ipdb.__main__ import set_trace, post_mortem, pm, run             # noqa
+from ipdb.__main__ import runcall, runeval, launch_ipdb_on_exception  # noqa
 
-pm                       # please pyflakes
-post_mortem              # please pyflakes
-run                      # please pyflakes
-runcall                  # please pyflakes
-runeval                  # please pyflakes
-set_trace                # please pyflakes
-launch_ipdb_on_exception # please pyflakes
-
-from ipdb.stdout import sset_trace, spost_mortem, spm, slaunch_ipdb_on_exception
-
-spm                       # please pyflakes
-spost_mortem              # please pyflakes
-sset_trace                # please pyflakes
-slaunch_ipdb_on_exception # please pyflakes
+from ipdb.stdout import sset_trace, spost_mortem, spm                 # noqa
+from ipdb.stdout import slaunch_ipdb_on_exception                     # noqa
