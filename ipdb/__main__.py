8c21cbdf66bd888c8e247e3d0d147f51f54b3e99
Godefroid Chapelle <gotcha@bubblenet.be>
2020-06-23 15:46:23 +0200

    Preparing release 0.13.3

--- ipdb/__main__.py
+++ ipdb/__main__.py
@@ -10,7 +10,7 @@ import sys
 
 from contextlib import contextmanager
 
-__version__ = '0.13.3.dev0'
+__version__ = '0.13.3'
 
 from IPython import get_ipython
 from IPython.core.debugger import BdbQuit_excepthook
