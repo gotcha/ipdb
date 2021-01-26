95b7159241c487e68e7fb1282ded09fa9dc4b225
David S <d1618033@gmail.com>
2021-01-26 22:26:35 +0200

    Fix ipython version for python 3.6

--- setup.py
+++ setup.py
@@ -58,7 +58,10 @@ setup(name='ipdb',
       extras_require={
           ':python_version == "2.7"': ['ipython >= 5.1.0, < 6.0.0'],
           # No support for python 3.0, 3.1, 3.2.
-          ':python_version >= "3.4"': ['ipython >= 5.1.0'],
+          ':python_version == "3.4"': ['ipython >= 6.0.0, < 7.0.0'],
+          ':python_version == "3.5"': ['ipython >= 7.0.0, < 7.10.0'],
+          ':python_version == "3.6"': ['ipython >= 7.10.0, < 7.17.0'],
+          ':python_version > "3.6"': ['ipython >= 7.17.0'],
       },
       tests_require=[
           'mock'
