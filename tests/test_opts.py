706782d3caba2d0816e0d39c394fb9b771d766dd
Adam Johnson <me@adamj.eu>
2021-03-15 18:21:41 +0000

    Avoid instantiating IPython on import
    Fixes #223.

--- tests/test_opts.py
+++ tests/test_opts.py
@@ -14,7 +14,7 @@ from getopt import GetoptError
 from ipdb.__main__ import main
 
 
-@patch('ipdb.__main__.debugger_cls')
+@patch('ipdb.__main__._get_debugger_cls')
 class OptsTest(unittest.TestCase):
     def set_argv(self, *argv):
         argv_patch = patch('ipdb.__main__.sys.argv', argv)
@@ -22,26 +22,26 @@ class OptsTest(unittest.TestCase):
         self.addCleanup(argv_patch.stop)
 
     @patch('ipdb.__main__.sys.version_info', (3, 7))
-    def test_debug_module_script(self, debugger_cls):
+    def test_debug_module_script(self, get_debugger_cls):
         module_name = 'my_buggy_module'
         self.set_argv('ipdb', '-m', module_name)
 
         main()
 
-        debugger = debugger_cls.return_value
+        debugger = get_debugger_cls.return_value.return_value
         debugger._runmodule.assert_called_once_with(module_name)
 
     @patch('ipdb.__main__.os.path.exists')
-    def test_debug_script(self, exists, debugger_cls):
+    def test_debug_script(self, exists, get_debugger_cls):
         script_name = 'my_buggy_script'
         self.set_argv('ipdb', script_name)
 
         main()
 
-        debugger = debugger_cls.return_value
+        debugger = get_debugger_cls.return_value.return_value
         debugger._runscript.assert_called_once_with(script_name)
 
-    def test_option_m_fallback_on_py36(self, debugger_cls):
+    def test_option_m_fallback_on_py36(self, get_debugger_cls):
         self.set_argv('ipdb', '-m', 'my.module')
         with patch('ipdb.__main__.sys.version_info', (3, 6)):
             with self.assertRaises(GetoptError):
