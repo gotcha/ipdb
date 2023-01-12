49caa4011fa596a66c54ed2e9ce9748ebaf43cbc
Andrew Zwicky <andrew.zwicky@gmail.com>
2023-01-12 15:19:32 -0600

    Update tests.yml
    Add separate tests for 3.11+, refactor hasattr call to call proper 3.11+ Pdb attribute.

--- tests/test_opts.py
+++ tests/test_opts.py
@@ -4,7 +4,10 @@
 # Redistributable under the revised BSD license
 # https://opensource.org/licenses/BSD-3-Clause
 
+import sys
 import unittest
+import os
+
 try:
     from unittest.mock import patch
 except ImportError:
@@ -21,6 +24,10 @@ class OptsTest(unittest.TestCase):
         argv_patch.start()
         self.addCleanup(argv_patch.stop)
 
+    @unittest.skipIf(
+        sys.version_info[0] == 3 and sys.version_info[1] >= 11,
+        ">3.11 requires different test",
+    )
     @patch('ipdb.__main__.sys.version_info', (3, 7))
     def test_debug_module_script(self, get_debugger_cls):
         module_name = 'my_buggy_module'
@@ -31,6 +38,10 @@ class OptsTest(unittest.TestCase):
         debugger = get_debugger_cls.return_value.return_value
         debugger._runmodule.assert_called_once_with(module_name)
 
+    @unittest.skipIf(
+        sys.version_info[0] == 3 and sys.version_info[1] >= 11,
+        ">3.11 requires different test",
+    )
     @patch('ipdb.__main__.os.path.exists')
     def test_debug_script(self, exists, get_debugger_cls):
         script_name = 'my_buggy_script'
@@ -40,6 +51,33 @@ class OptsTest(unittest.TestCase):
 
         debugger = get_debugger_cls.return_value.return_value
         debugger._runscript.assert_called_once_with(script_name)
+
+    @unittest.skipIf(
+        sys.version_info[0] != 3 or sys.version_info[1] < 11,
+        "<3.11 requires different test",
+    )
+    def test_debug_module_script_3_11(self, get_debugger_cls):
+        module_name = 'my_buggy_module_3_11'
+        self.set_argv('ipdb', '-m', module_name)
+
+        main()
+
+        debugger = get_debugger_cls.return_value.return_value
+        debugger._run.assert_called_once_with(module_name)
+
+    @unittest.skipIf(
+        sys.version_info[0] != 3 or sys.version_info[1] < 11,
+        "<3.11 requires different test",
+    )
+    @patch('ipdb.__main__.os.path.exists')
+    def test_debug_script_3_11(self, exists, get_debugger_cls):
+        script_name = 'my_buggy_script_3_11'
+        self.set_argv('ipdb', script_name)
+
+        main()
+
+        debugger = get_debugger_cls.return_value.return_value
+        debugger._run.assert_called_once_with(os.path.join(os.getcwd(), script_name))
 
     def test_option_m_fallback_on_py36(self, get_debugger_cls):
         self.set_argv('ipdb', '-m', 'my.module')
